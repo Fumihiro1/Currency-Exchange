f2f7de16d4f4113cde78dd6ec391cb7d1841b370
lroygard <lroygard@gmail.com>
2024-08-28 10:41:21 +1200

    Create graph

---
+++ Currency-Exchange/src/currency/exchange/Graph.java
@@ -0,0 +1,14 @@
+package currency.exchange;
+
+import java.util.ArrayList;
+
+public class Graph {
+	int no_Vertices;
+	ArrayList<Edge> edges;
+	
+	Graph(int vertices) {
+		this.no_Vertices = vertices;
+		edges = new ArrayList<>();
+	}
+	
+}
